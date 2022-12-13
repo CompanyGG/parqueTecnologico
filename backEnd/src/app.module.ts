9acb7e3376e16ede19124a6a6cbb6aebaaf52c8b
Diaszano <lucasdiiassantos@gmail.com>
2022-12-13 23:23:48 +0000

    gerado resource da reserva

--- backEnd/src/app.module.ts
+++ backEnd/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReservaModule } from './reserva/reserva.module';
 
 @Module({
-  imports: [],
+  imports: [ReservaModule],
   controllers: [AppController],
   providers: [AppService],
 })
