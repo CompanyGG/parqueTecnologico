6cb584200d5677c80a6fdd2078e11b802e6e47dd
Diaszano <lucasdiiassantos@gmail.com>
2022-12-14 15:52:47 +0000

    add prisma modulo

--- backEnd/src/app.module.ts
+++ backEnd/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReservaModule } from './reserva/reserva.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ReservaModule],
+  imports: [ReservaModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
