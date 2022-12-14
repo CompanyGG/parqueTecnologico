8f597fde5f9cca976001249de09d61ebe27c4be0
Diaszano <lucasdiiassantos@gmail.com>
2022-12-14 16:34:03 +0000

    criado user dto

--- backEnd/src/user/dto/create-user.dto.ts
+++ backEnd/src/user/dto/create-user.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { User } from '../entities/user.entity';
+
+export class CreateUserDto extends User {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @IsNotEmpty()
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  senha: string;
+
+  @IsString()
+  @IsNotEmpty()
+  nome: string;
+}
