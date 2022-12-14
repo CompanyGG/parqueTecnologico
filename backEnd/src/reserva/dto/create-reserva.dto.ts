16cb462704ac857ff1ab130ef1dcb66afb2afaf4
Diaszano <lucasdiiassantos@gmail.com>
2022-12-14 01:17:09 +0000

    create data dto

--- backEnd/src/reserva/dto/create-reserva.dto.ts
+++ backEnd/src/reserva/dto/create-reserva.dto.ts
@@ -1,1 +1,7 @@
-export class CreateReservaDto {}
+import { Reserva } from '../entities/reserva.entity';
+import { IsBoolean, IsOptional } from 'class-validator';
+export class CreateReservaDto extends Reserva {
+  @IsBoolean()
+  @IsOptional()
+  confirmado?: boolean;
+}
--- backEnd/src/reserva/entities/reserva.entity.ts
+++ backEnd/src/reserva/entities/reserva.entity.ts
@@ -1,1 +1,8 @@
-export class Reserva {}
+import { Prisma } from '@prisma/client';
+
+export class Reserva implements Prisma.ReservaUncheckedCreateInput {
+  id?: string;
+  confirmado?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
