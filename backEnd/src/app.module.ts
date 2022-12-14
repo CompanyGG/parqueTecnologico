410b4d6b6facc5aad3b9ea04b3d247a23796829b
Diaszano <lucasdiiassantos@gmail.com>
2022-12-14 16:05:07 +0000

    criado auth

--- backEnd/src/app.module.ts
+++ backEnd/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReservaModule } from './reserva/reserva.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ReservaModule, PrismaModule],
+  imports: [ReservaModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
