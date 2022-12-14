54a2a4f3befa30cf7fa176e8de1ff910bd84cfa1
Diaszano <lucasdiiassantos@gmail.com>
2022-12-14 16:47:29 +0000

    add prisma no user

--- backEnd/src/reserva/reserva.module.ts
+++ backEnd/src/reserva/reserva.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ReservaController } from './reserva.controller';
 import { ReservaService } from './reserva.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ReservaController],
   providers: [ReservaService, PrismaService],
 })
