e183ea634b4a78e567a00e91f18d1242fe4b16ae
Diaszano <lucasdiiassantos@gmail.com>
2022-12-14 01:26:00 +0000

    add da forma que eu gosto de métodos privados

--- backEnd/src/reserva/reserva.controller.ts
+++ backEnd/src/reserva/reserva.controller.ts
@@ -1,34 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { ReservaService } from './reserva.service';
 import { CreateReservaDto } from './dto/create-reserva.dto';
 import { UpdateReservaDto } from './dto/update-reserva.dto';
 
 @Controller('reserva')
 export class ReservaController {
-  constructor(private readonly reservaService: ReservaService) {}
+  constructor(private readonly __reservaService: ReservaService) {}
 
   @Post()
-  create(@Body() createReservaDto: CreateReservaDto) {
-    return this.reservaService.create(createReservaDto);
+  public async create(@Body() createReservaDto: CreateReservaDto) {
+    return this.__reservaService.create(createReservaDto);
   }
 
   @Get()
-  findAll() {
-    return this.reservaService.findAll();
+  public async findAll() {
+    return this.__reservaService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.reservaService.findOne(+id);
+  public async findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.__reservaService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateReservaDto: UpdateReservaDto) {
-    return this.reservaService.update(+id, updateReservaDto);
+  public async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateReservaDto: UpdateReservaDto,
+  ) {
+    return this.__reservaService.update(id, updateReservaDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.reservaService.remove(+id);
+  public async remove(@Param('id', ParseIntPipe) id: number) {
+    return this.__reservaService.remove(id);
   }
 }
