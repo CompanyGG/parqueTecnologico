331386f14c530c2e85eb8a61e17e64e3b5d5765b
Diaszano <lucasdiiassantos@gmail.com>
2022-12-14 16:18:40 +0000

    criado modulo do user

--- backEnd/src/app.module.ts
+++ backEnd/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ReservaModule } from './reserva/reserva.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ReservaModule, PrismaModule, AuthModule],
+  imports: [ReservaModule, PrismaModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
